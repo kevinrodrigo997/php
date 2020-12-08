2691f1edbf1be200612572a01a6bef6a5e89fa3e
kevinrodrigo997 <70495394+kevinrodrigo997@users.noreply.github.com>
2020-12-08 13:19:15 +0800

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -5,9 +5,13 @@ if($con->auth()){
     switch (@$_GET['mod']){
         case 'tamu':
             include_once 'controller/tamu.php';
+        case 'karyawan':
+            include_once 'controller/karyawan.php';
+        case 'kamar':
+            include_once 'controller/kamar.php';
             break;
         default:
-        include_once 'controller/tamu.php';
+            include_once 'controller/tamu.php';
     }
 }else{
     include_once 'controller/c_login.php';
